8da95ed7fcd175b093e7f3b7e5bb6be24c473fe8
Danielpernnasc <daniel.nascimento@smartezy.com.br>
2025-06-16 08:57:55 -0300

    retirada dos lazy louad

--- projects/shell/src/app/app-routing.module.ts
+++ projects/shell/src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { loadRemoteModule } from '@angular-architects/module-federation';
 import { authGuard } from './guard/auth.guard';
 import { LoginComponent } from './login/login.component';
+import { CadastroComponent } from 'projects/mfe-cadastro/src/app/cadastro/cadastro.component';
+import { SucessoComponent } from 'projects/mfe-sucesso/src/app/sucesso/sucesso.component';
 
 const routes: Routes = [
   {
@@ -11,22 +13,12 @@ const routes: Routes = [
   },
   {
     path: 'cadastro',
-    loadChildren: () =>
-      loadRemoteModule({
-        type: 'module',
-        remoteEntry: 'http://18.217.92.231/remoteEntry.js',
-        exposedModule: './CadastroModule'
-      }).then(m => m.CadastroModule),
+    component: CadastroComponent
   },
   {
     path: 'sucesso',
-    canActivate: [authGuard],
-    loadChildren: () =>
-      loadRemoteModule({
-        type: 'module',
-        remoteEntry: 'http://18.217.92.231/remoteEntry.js',
-        exposedModule: './SucessoModule'
-      }).then(m => m.SucessoModule),
+    component: SucessoComponent,
+    canActivate: [authGuard]
   },
 
   { path: '', redirectTo: 'login', pathMatch: 'full' },
