58e13c1bd3c258f4cd7774b62af7410605e93ce1
Danielpernnasc <daniel.nascimento@smartezy.com.br>
2025-06-15 21:05:08 -0300

    atualizando para subida aws

--- projects/shared-lib/src/lib/authentic/service/auth.service.ts
+++ projects/shared-lib/src/lib/authentic/service/auth.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Config } from 'projects/shared-lib/src/urlconfig/url.config';
 import { catchError, map, Observable, of, tap } from 'rxjs';
 
-const API_URL = 'http://localhost:8080';
+const API_URL = Config.urlApi;
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +14,7 @@ export class AuthService {
   constructor(private http: HttpClient) {}
 
   login(email: string, password: string): Observable<boolean> {
-    return this.http.post<any>(`${API_URL}/clientes/login`, { email, password }).pipe(
+    return this.http.post<any>(`${API_URL}/login`, { email, password }).pipe(
       tap(response => {
         if (response?.token) {
           localStorage.setItem('authToken', response.token); // Salva o token
@@ -23,7 +24,7 @@ export class AuthService {
       catchError(() => of(false))
     );
   }
-  
+
 
   logout(): void {
     localStorage.removeItem('authToken');
